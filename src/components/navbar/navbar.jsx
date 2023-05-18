84ed2c35186ec5549cb4cc1686cfd325ed363d7a
PedroGLL27 <pedrolou27@gmail.com>
2023-05-18 21:43:08 +0100

    oaoaoaoaoao

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import bars from "../../assets/bars.png";
+
+const Navbar = () => {
+  const [showOptions, setShowOptions] = useState(false);
+
+  const handleClickLogo = () => {
+    setShowOptions(!showOptions);
+  };
+
+  return (
+    <nav>
+      <div className="left-section">
+        <img src={bars} alt="bars" onClick={handleClickLogo} />
+        {showOptions && (
+          <ul className="options-list">
+            {/* Opções de menu */}
+            <li>Opção 1</li>
+            <li>Opção 2</li>
+            <li>Opção 3</li>
+          </ul>
+        )}
+      </div>
+      <div className="right-section">
+        <div className="user-profile">
+          <div className="profile-image">
+            {/* Imagem de perfil */}
+            <img src="" alt="Foto de perfil" />
+          </div>
+          <span className="username">Nome de usuário</span>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
